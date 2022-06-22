777bbb196b8f48ccc87d91787cf9d5cd90032094
CarlosLoeza <cloeza1@mail.ccsf.edu>
2022-06-22 12:50:42 -0700

    Init repo

---
+++ AR-SchoolTourGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AR-SchoolTourGuide
+//
+//  Created by Carlos Loeza on 6/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
