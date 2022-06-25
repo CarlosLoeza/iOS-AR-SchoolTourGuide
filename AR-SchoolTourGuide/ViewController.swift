d829078aa730a2269c65771f6437fed701393977
CarlosLoeza <cloeza1@mail.ccsf.edu>
2022-06-25 13:53:01 -0700

    Creating select starting and destination points
    User will be able to select where they are starting from and where they are going. This will allows us to create an AR map in the future.

--- AR-SchoolTourGuide/ViewController.swift
+++ AR-SchoolTourGuide/ViewController.swift
@@ -7,7 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        <#code#>
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
+    }
+    
+    // create a picker roller view so user can select a starting point from list
+    @IBOutlet weak var startingPointPickerView: UIButton!
+    // user can use their current location
+    @IBOutlet weak var userLocation: UIButton!
+    // list of starting and destination points
+    var locations = [
+        "J. Paul Leonard Library": String(),
+        "Cesar Chavez Student Center": String(),
+        "Creative Arts": String(),
+        "Humanities": String(),
+        "Business": String()
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
