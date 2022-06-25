55174e5fd0c76073a81e4272336472a9d6470527
CarlosLoeza <cloeza1@mail.ccsf.edu>
2022-06-25 14:22:44 -0700

    Working on picker roller for starting point
    Picker roller contains items but it shows "?" on the when I scroll. For example instead of showing me a list of locations, it contains a list of strings containing "?"

--- AR-SchoolTourGuide/ViewController.swift
+++ AR-SchoolTourGuide/ViewController.swift
@@ -8,13 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        <#code#>
-    }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        <#code#>
-    }
+    // screen width and height for picker roller
+    let screenWidth = UIScreen.main.bounds.width-10
+    let screenHeight = UIScreen.main.bounds.height / 3
+    // selectedRow
+    var selectedRow = 0
     
     // create a picker roller view so user can select a starting point from list
     @IBOutlet weak var startingPointPickerView: UIButton!
@@ -26,16 +25,62 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         "Cesar Chavez Student Center": String(),
         "Creative Arts": String(),
         "Humanities": String(),
-        "Business": String()
+        "Business": String(),
     ]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
+    @IBAction func pickerRoll(_ sender: Any) {
+        let vc = UIViewController()
+        vc.preferredContentSize = CGSize(width: screenWidth, height: screenHeight)
+        let pickerView = UIPickerView(frame: CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight))
+        pickerView.dataSource = self
+        pickerView.selectRow(selectedRow, inComponent: 0, animated: false)
+        vc.view.addSubview(pickerView)
+        pickerView.centerXAnchor.constraint(equalTo: vc.view.centerXAnchor).isActive = true
+        pickerView.centerYAnchor.constraint(equalTo: vc.view.centerYAnchor).isActive = true
+        
+        let alert = UIAlertController(title: "Select Starting Point", message: "", preferredStyle: .actionSheet)
+        alert.setValue(vc, forKey: "contentViewController")
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {(UIAlertAction) in }))
+        
+        alert.addAction(UIAlertAction(title: "Select", style: .default, handler: {(UIAlertAction) in
+            self.selectedRow = pickerView.selectedRow(inComponent: 0)
+            let selected = Array(self.locations)[self.selectedRow]
+            let starting = selected.key
+            self.startingPointPickerView.setTitle(starting, for: .normal)
+            
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
     
     
+
     
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        locations.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 30))
+        label.text = Array(locations)[row].key
+        label.sizeToFit()
+        return label
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 30
+    }
 
 
 }
