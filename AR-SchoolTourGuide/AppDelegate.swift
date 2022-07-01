7495bf98bc1da280dd1fb61dffc06da303ccb627
CarlosLoeza <cloeza1@mail.ccsf.edu>
2022-07-01 12:52:57 -0700

    Revert "Merge pull request #13 from CarlosLoeza/feature-AR-coaching-overlay"
    This reverts commit e5fcb3cd252f4ba2a022c4aef719a0b628bfd6a9, reversing
    changes made to db74e4a8faef247f6719f428a2cb0f32da262d2d.

--- AR-SchoolTourGuide/AppDelegate.swift
+++ AR-SchoolTourGuide/AppDelegate.swift
@@ -6,19 +6,14 @@
 //
 
 import UIKit
-import ARKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
+
+
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        /* Check whether the device supports geotracking. Present
-         an error-message view controller, if not. */
-        if !ARGeoTrackingConfiguration.isSupported {
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "unsupportedDeviceMessage")
-        }
+        // Override point for customization after application launch.
         return true
     }
 
