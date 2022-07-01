934b9fe999324a0ad41db26b0bcdabc6c61891d0
CarlosLoeza <cloeza1@mail.ccsf.edu>
2022-07-01 13:02:52 -0700

    Revert "Merge branch 'Development' into feature-AR-coaching-overlay"
    This reverts commit 7db2a756cd4b3b7db34874308a7def4a36b5e5d5, reversing
    changes made to 0e18ef89bed4398abae199848caaa902816c981b.

--- AR-SchoolTourGuide/AppDelegate.swift
+++ AR-SchoolTourGuide/AppDelegate.swift
@@ -10,8 +10,9 @@ import ARKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
 
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         /* Check whether the device supports geotracking. Present
          an error-message view controller, if not. */
--- AR-SchoolTourGuideTests/AR_SchoolTourGuideTests.swift
+++ AR-SchoolTourGuideTests/AR_SchoolTourGuideTests.swift
@@ -25,6 +25,8 @@ class AR_SchoolTourGuideTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
+    
+ 
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
