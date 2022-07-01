db74e4a8faef247f6719f428a2cb0f32da262d2d
CarlosLoeza <cloeza1@mail.ccsf.edu>
2022-07-01 12:12:35 -0700

    Revert "Merge branch 'feature-map-paths' into Development"
    This reverts commit a614b1629724f53e6cb05981737328beb735a6d3, reversing
    changes made to a3d4c152e7899fc90be1bd6428389407c25b608d.

--- AR-SchoolTourGuideTests/AR_SchoolTourGuideTests.swift
+++ AR-SchoolTourGuideTests/AR_SchoolTourGuideTests.swift
@@ -26,7 +26,6 @@ class AR_SchoolTourGuideTests: XCTestCase {
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
 
-
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
